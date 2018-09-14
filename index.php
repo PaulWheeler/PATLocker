4fbea373c9f4d16da4c1fe77ff779714dce6c31c
Paul Wheeler <paul.stuart.wheeler@gmail.com>
2018-09-14 21:13:33 +0100

    Test commit from pauls Atom

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>
 	<div style="width:950px; margin: 0 auto;">
 			<h1 class="Titillium">PAT Locker:</h1>
-			<h2 class="Titillium">A resource for Portable Appliance Test personnel and appliance owners:</h2>
+			<h2 class="Titillium">An open source implementation  resource for Portable Appliance Test personnel and appliance owners:</h2>
 
 		<a href="choose_account.php"><img title="Login / Join" alt="Filing cabinets full of PAT test data" src="images/Patlogo.png">Login / Join</a>
 		<br>
