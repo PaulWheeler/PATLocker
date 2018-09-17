3452aceb07b5a69b911ad48b40ce3b1dbdc10b54
Paul Wheeler <paul.stuart.wheeler@gmail.com>
2018-09-17 20:53:53 +0100

    Alter top of site messages to display that the code is now open source

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <body>
 	<div style="width:950px; margin: 0 auto;">
 			<h1 class="Titillium">PAT Locker:</h1>
-			<h2 class="Titillium">An open source implementation  resource for Portable Appliance Test personnel and appliance owners:</h2>
-
+		
+			<h2 class="Titillium">An open source resource for Portable Appliance Test personnel and appliance owners:</h2>
+			<!-- test comment-->
 		<a href="choose_account.php"><img title="Login / Join" alt="Filing cabinets full of PAT test data" src="images/Patlogo.png">Login / Join</a>
 		<br>
 		<br>
--- plugin/header4button.php
+++ plugin/header4button.php
@@ -1,8 +1,8 @@
 
 <div style="width:950px; height:100%; margin: 0 auto;">
-  <h1>PAT Locker: a resource for Portable Appliance Test personnel and appliance owners:</h1>
+  <h1>PAT Locker: an open source resource for PAT personnel and appliance owners:</h1>
   <!--Menu start-->
   <div id="MenuContainer"><?php include('plugin/menu4button.php'); ?></div>
   <br><br>
   <!--Menu end-->
-<?php require_once 'braintree_setup.php';?>
+<?php //require_once 'braintree_setup.php';?>
